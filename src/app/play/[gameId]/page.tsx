a5e9bef68ddf1a90362e66302643b42ac73435f2
pablocorrea81 <91747054+pablocorrea81@users.noreply.github.com>
2025-11-11 15:06:52 -0300

    Update page.tsx

--- src/app/play/[gameId]/page.tsx
+++ src/app/play/[gameId]/page.tsx
@@ -80,7 +80,7 @@ export default async function PlayPage({ params }: PlayPageProps) {
   if (!userIsPlayer && !rawGame.is_bot_game) {
     if (rawGame.status === "waiting" && rawGame.player_2_id === null) {
       const { data: joinedGame, error: joinError } = (await supabaseAdmin
-        .from<Database["public"]["Tables"]["games"]["Row"]>("games")
+        .from("games")
         .update({
           player_2_id: session.user.id,
           status: "in_progress",
